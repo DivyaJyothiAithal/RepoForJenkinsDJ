022b8b56f43c7acc3c1bce055060966ed085b66c
DivyaJyothiAithal <djbairi@gmail.com>
2024-12-25 11:29:00 +0530

    Push

--- PrjForJenkins/src/main/java/Generic_Utilities/BaseClass.java
+++ PrjForJenkins/src/main/java/Generic_Utilities/BaseClass.java
@@ -17,79 +17,81 @@ import org.testng.annotations.BeforeTest;
 
 
 public class BaseClass {
-	public WebDriver driver;
-
-	@BeforeSuite
-	public void BefSuite()
-	{
-		System.out.println("Database connection Successful");
-	}
-	
-	@BeforeTest
-	public void BefTest()
-	{
-		System.out.println("Parallel Execution Started");
-	}
-	
-
-	@BeforeClass
-	public void BefClass() throws Throwable // (String browser)throws Throwable
-	{	
-		//String BROWSER=System.getProperty("browser");
-		String BROWSER="edge";
-		if(BROWSER.equalsIgnoreCase("edge"))
-		driver=new EdgeDriver();
-		
-		
-	}
 	
 	
-	@BeforeMethod
-	public void BefMethod()throws Throwable
-	{
+//	public WebDriver driver;
 
-		System.out.println("Before Method started");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-		
-		driver.get("http://localhost:8888/");
-		
-//		String UNAME=System.getProperty("uname");
-//		String PASS=System.getProperty("pwd");
-		
-		String UNAME="admin";
-		String PASS="admin";
-		driver.findElement(By.name("user_name")).sendKeys(UNAME);
-		driver.findElement(By.name("user_password")).sendKeys(PASS);
-		driver.findElement(By.id("submitButton")).click();
-
-	}
-
-	@AfterMethod
-	public void AftMethod()
-	{
-		System.out.println("After Method started");
-	}
-	
-	@AfterClass
-	public void AftClass() throws Throwable
-	{
-		System.out.println("After class started");
-		driver.quit();
-	}
-
-	@AfterTest
-	public void AftTest()
-	{
-		System.out.println("Parallel Execution Closed");
-		
-	}
-	
-	@AfterSuite
-	public void AftSuite()
-	{
-		System.out.println("Database connection Closed");
-	}
+//	@BeforeSuite
+//	public void BefSuite()
+//	{
+//		System.out.println("Database connection Successful");
+//	}
+//	
+//	@BeforeTest
+//	public void BefTest()
+//	{
+//		System.out.println("Parallel Execution Started");
+//	}
+//	
+//
+//	@BeforeClass
+//	public void BefClass() throws Throwable // (String browser)throws Throwable
+//	{	
+//		//String BROWSER=System.getProperty("browser");
+//		String BROWSER="edge";
+//		if(BROWSER.equalsIgnoreCase("edge"))
+//		driver=new EdgeDriver();
+//		
+//		
+//	}
+//	
+//	
+//	@BeforeMethod
+//	public void BefMethod()throws Throwable
+//	{
+//
+//		System.out.println("Before Method started");
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+//		
+//		driver.get("http://localhost:8888/");
+//		
+////		String UNAME=System.getProperty("uname");
+////		String PASS=System.getProperty("pwd");
+//		
+//		String UNAME="admin";
+//		String PASS="admin";
+//		driver.findElement(By.name("user_name")).sendKeys(UNAME);
+//		driver.findElement(By.name("user_password")).sendKeys(PASS);
+//		driver.findElement(By.id("submitButton")).click();
+//
+//	}
+//
+//	@AfterMethod
+//	public void AftMethod()
+//	{
+//		System.out.println("After Method started");
+//	}
+//	
+//	@AfterClass
+//	public void AftClass() throws Throwable
+//	{
+//		System.out.println("After class started");
+//		driver.quit();
+//	}
+//
+//	@AfterTest
+//	public void AftTest()
+//	{
+//		System.out.println("Parallel Execution Closed");
+//		
+//	}
+//	
+//	@AfterSuite
+//	public void AftSuite()
+//	{
+//		System.out.println("Database connection Closed");
+//	}
 
 }
 
--- PrjForJenkins/src/test/java/Demo/vTiger.java
+++ PrjForJenkins/src/test/java/Demo/vTiger.java
@@ -1,19 +1,27 @@
 package Demo;
 
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import Generic_Utilities.BaseClass;
+//import Generic_Utilities.BaseClass;
 
-public class vTiger extends BaseClass
+public class vTiger //extends BaseClass
 {
-	@Test
+@Test
 	public void method()
 	{
 
 		System.out.println("vTiger for Jenkins Running.............");
 		
-	
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		
+		driver.get("http://localhost:8888/");
 	}
 
 	
